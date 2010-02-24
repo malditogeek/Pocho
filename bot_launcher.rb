7ff49bb81e59df905b6a7025cc0b2ec8d7113280
ralph <ralph@rvdh.de>
2010-02-24 20:04:18 +0100

    mv config.yml out of the way so it does not conflict with local installs of this app

--- bot_launcher.rb
+++ bot_launcher.rb
@@ -2,6 +2,13 @@
 
 require 'yaml'
 require File.dirname(File.expand_path(__FILE__)) + '/pocho_xmpp'
-config = YAML.load_file('config.yml')
+
+begin
+  YAML.load_file('config.yml')
+rescue Errno::ENOENT
+  puts 'No config.yml file found, which is needed to run this script. You can copy config.yml.example as a starting point.'
+  Process.exit
+end
+
 bot = PochoTheRobot.new config[:user], config[:password], config[:rooms]
 bot.connect!
